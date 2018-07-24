f57e7e7aa4b752c55f991c0503a18019f3ce4016
Bruno Yudi TIyoda <bruno_tiyoda@hotmail.com>
2018-07-24 18:34:16 -0300

    Ajuste BD

--- src/main/java/configs/DatabaseConfig.java
+++ src/main/java/configs/DatabaseConfig.java
@@ -1,4 +1,3 @@
-/*
 package configs;
 
 import org.springframework.context.annotation.Bean;
@@ -14,8 +13,37 @@ import java.util.Properties;
 
 @Configuration
 public class DatabaseConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
 
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUsername("root");
+        dataSource.setPassword("q1w2e3r4");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/power");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
+        factoryBean.setDataSource(dataSource);
+
+        Properties propriedadesHibernate = new Properties();
+        propriedadesHibernate.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        propriedadesHibernate.setProperty("hibernate.show_sql", "true");
+        propriedadesHibernate.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        factoryBean.setJpaProperties(propriedadesHibernate);
+
+        factoryBean.setPackagesToScan("model");
+
+        return factoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        entityManagerFactory.createEntityManager();
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
-*/
+
